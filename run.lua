8d64a132a729894008c0aa0c9331f1254e2eb9e3
jmolin1 <jmolin1@jhu.edu>
2013-11-11 09:52:31 -0500

    Update run.lua

--- run.lua
+++ run.lua
@@ -22,33 +22,44 @@ require 'setup_filters'
 torch.setdefaulttensortype('torch.DoubleTensor')
 
 MAX_FILTER_SIZE = 9;
+NUM_LEVELS = 1;
 
 ------------------
 --GET PARAMETERS--
 ------------------
-local defaultParameters = getDefaultParameters(10);
+local defaultParameters = getDefaultParameters(NUM_LEVELS);
 
 -------------------------------------------------------------------------
 --SETUP FILTERS AND GET LOCATIONS IN NEURAL NETWORK SPATIAL CONVOLUTION--
 -------------------------------------------------------------------------
 local EVMSK,ODDMSK,GABORMSK,CSMSK,VMSK1,VMSK2;
-conv,EVMSK,ODDMSK,GABORMSK,CSMSK,VMSK1,VMSK2 = createFilters(defaultParameters,MAX_FILTER_SIZE);
-
+conv1,conv2,EVMSK,ODDMSK,GABORMSK,CSMSK,VMSK1,VMSK2 = createFilters(defaultParameters,MAX_FILTER_SIZE);
 
 local im = image.loadJPG('/home/jamal/ProtoObject/soccer.jpg')
 
+timerr = torch.Timer();
 im = normalizeImage(im);
+print('NormalizeImage = ' .. timerr:time().real);
 
 --Generate channels from image
+--timerr = torch.Timer();
 local im_channels = generateChannels(im,defaultParameters,GABORMSK);
+--print('GenerateChannels = ' .. timerr:time().real);
 
+timerr = torch.Timer();
 local b1Pyr_final, b2Pyr_final = makeBorderOwnership(im_channels,defaultParameters,EVMSK,ODDMSK,CSMSK,VMSK1,VMSK2);
+print('MakeBorderOwnership = ' .. timerr:time().real);
 
-
+timerr = torch.Timer();
 local gPyr_final = makeGrouping(b1Pyr_final, b2Pyr_final, defaultParameters);
---print('MakeGrouping = ' .. timerr:time().real);
+print('MakeGrouping = ' .. timerr:time().real);
 
-
+--image.display{image = gPyr_final[1].subtype[1][1].data}
+timerr = torch.Timer();
 local h_final = ittiNorm(gPyr_final,1);
+print('IttiNormalization = ' .. timerr:time().real);
 
+--save data to file
+writeToFile(h_final.data,'sal_out_map.txt');
+-- make sure the data is written
 image.display{image = h_final.data}
